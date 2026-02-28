aa2fc54a6a24a3815214b0e2267712fd02969889
profjarbasinfinity <jarbas.lima@ifpb.edu.br>
2026-02-27 21:20:26 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,22 @@
-self.addEventListener('fetch', function(event) {
-    
-});
+{
+  "short_name": "InFinity",
+  "name": "InFinity Inglês Acadêmico",
+  "description": "Plataforma de aprendizado de Inglês - Prof. Jarbas",
+  "icons": [
+    {
+      "src": "logo-circulo-192.png",
+      "type": "image/png",
+      "sizes": "192x192"
+    },
+    {
+      "src": "logo-circulo-512.png",
+      "type": "image/png",
+      "sizes": "512x512"
+    }
+  ],
+  "start_url": "index.html",
+  "background_color": "#050816",
+  "display": "standalone",
+  "scope": "./",
+  "theme_color": "#2a5ff0"
+}
